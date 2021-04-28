aa0d5bc723c7f23d0645f8740b9f7c13478350b7
Gaia Molinaro <66551342+gaiamolinaro@users.noreply.github.com>
2021-04-28 21:29:26 +0200

    Add files via upload

--- js/website-scripts.js
+++ js/website-scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    $('.post-wrapper').slick({
-        dots: true,
+    $('.post-wrapper-writing').slick({
+        dots: false,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         centerMode: true,
         variableWidth: true,
-        nextArrow: $('.next'),
-        prevArrow: $('.prev'),
+        nextArrow: $('.writing-next'),
+        prevArrow: $('.writing-prev'),
         responsive: [
             {
                 breakpoint: 1024,
@@ -15,7 +15,45 @@ $(document).ready(function () {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                     infinite: true,
-                    dots: true
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.post-wrapper-collages').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        nextArrow: $('.collages-next'),
+        prevArrow: $('.collages-prev'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
                 }
             },
             {
