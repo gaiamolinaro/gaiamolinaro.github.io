0a692d192cd510e9fde23801b01aebbb6d0b6527
Gaia Molinaro <66551342+gaiamolinaro@users.noreply.github.com>
2021-04-30 15:02:44 +0200

    Update website-scripts.js

--- js/website-scripts.js
+++ js/website-scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    $('.post-wrapper-writing').slick({
+    $('.post-wrapper1').slick({
         dots: false,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         centerMode: true,
         variableWidth: true,
-        nextArrow: $('.writing-next'),
-        prevArrow: $('.writing-prev'),
+        nextArrow: $('.next1'),
+        prevArrow: $('.prev1'),
         responsive: [
             {
                 breakpoint: 1024,
@@ -37,15 +37,53 @@ $(document).ready(function () {
             // instead of a settings object
         ]
     });
-    $('.post-wrapper-collages').slick({
+    $('.post-wrapper2').slick({
         dots: false,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         centerMode: true,
         variableWidth: true,
-        nextArrow: $('.collages-next'),
-        prevArrow: $('.collages-prev'),
+        nextArrow: $('.next2'),
+        prevArrow: $('.prev2'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.post-wrapper3').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        nextArrow: $('.next3'),
+        prevArrow: $('.prev3'),
         responsive: [
             {
                 breakpoint: 1024,
