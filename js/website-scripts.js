ad442356305a60add00abd7a83cdef90394c0b04
Gaia Molinaro <66551342+gaiamolinaro@users.noreply.github.com>
2021-05-03 11:23:56 +0200

    Update website-scripts.js

--- js/website-scripts.js
+++ js/website-scripts.js
@@ -1,7 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
 $(document).ready(function () {
     $('.post-wrapper1').slick({
         dots: false,
@@ -118,8 +114,3 @@ $(document).ready(function () {
         ]
     });
 })
-jQuery(document).ready(function($){
-    jQuery('.post').lazyload({
-        effect:"fadeIn",
-    });
-});
